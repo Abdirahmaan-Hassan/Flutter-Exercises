5ffc65e61dd949dbaed5a2c7207fd0f43268bb49
Abdirahmaan <abdihdilbar@gmail.com>
2023-10-18 21:16:09 +0300

    Second Commit

---
+++ lib/maanka.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class maanka extends StatelessWidget {
+  const maanka({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
